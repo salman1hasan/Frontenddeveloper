203ae612c8f340bd2c2d9d5ece2443d76e75c832
Salman Hasan <sh8salmanhasan@gmail.com>
2023-03-20 17:30:11 -0400

    First Commit

---
+++ components/Header.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import Wrapper from './Wrapper';
+
+const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatmenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState('translate-y-0');
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0
+      transition-transform duration-300 ${show}`}
+    >
+      <Wrapper>wefwefwefwefwef</Wrapper>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/category/category1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const cateogry1 = () => {
+  return <div>cateogry1</div>;
+};
+
+export default cateogry1;
